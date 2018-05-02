2611ce9b23055d9c20b74f95f1872716930a001e
danieldidonato <36905486+danieldidonato@users.noreply.github.com>
2018-05-01 22:32:31 -0400

    Add files via upload
    update

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@ $('.js-modal').on($.modal.BEFORE_OPEN, (e, modal) => {
 
 
 
+
+const $giphContainer3 = $('.js-beachGif')
+const gifURL3 = 'http://api.giphy.com/v1/gifs/search?q=ocean&api_key=Nlu41GYoEb83vQiekoMQsuciJgIpkEQZ'
+$.get(gifURL3, response => {
+	const gifs3 = response.data;
+	// for (let i = 0; i < gifs.length; i++) {
+		console.log(gifs3[1])
+		const end = gifs3[0].images.downsized_medium.url;
+		$giphContainer3.prepend(`<img src="${end}">`)
+		// }
+	})
+
+
+
 const $giphContainer = $('.js-rainGif')
 const gifURL = 'http://api.giphy.com/v1/gifs/search?q=rain&api_key=Nlu41GYoEb83vQiekoMQsuciJgIpkEQZ'
 $.get(gifURL, response => {
@@ -21,34 +35,22 @@ $.get(gifURL, response => {
 const $giphContainer1 = $('.js-windGif')
 const gifURL1 = 'http://api.giphy.com/v1/gifs/search?q=field%20breeze&api_key=Nlu41GYoEb83vQiekoMQsuciJgIpkEQZ'
 $.get(gifURL1, response => {
-	const gifs = response.data;
+	const gifs1 = response.data;
 	// for (let i = 0; i < gifs.length; i++) {
-		console.log(gifs[1])
-		const end = gifs[1].images.downsized_medium.url;
+		console.log(gifs1[1])
+		const end = gifs1[1].images.downsized_medium.url;
 		$giphContainer1.prepend(`<img src="${end}">`)
 		// }
 	})
 
 
-const $giphContainer2 = $('.js-waterGif')
-const gifURL2 = 'http://api.giphy.com/v1/gifs/search?q=rushing%20water&api_key=Nlu41GYoEb83vQiekoMQsuciJgIpkEQZ'
+const $giphContainer2 = $('.js-rushingGif')
+const gifURL2 = 'http://api.giphy.com/v1/gifs/search?q=nature%20waters&api_key=Nlu41GYoEb83vQiekoMQsuciJgIpkEQZ'
 $.get(gifURL2, response => {
-	const gifs = response.data;
+	const gifs2 = response.data;
 	// for (let i = 0; i < gifs.length; i++) {
-		console.log(gifs[1])
-		const end = gifs[0].images.downsized_medium.url;
+		console.log(gifs2[1])
+		const end = gifs2[0].images.downsized_medium.url;
 		$giphContainer2.prepend(`<img src="${end}">`)
-		// }
-	})
-
-
-const $giphContainer3 = $('.js-beachGif')
-const gifURL3 = 'http://api.giphy.com/v1/gifs/search?q=ocean&api_key=Nlu41GYoEb83vQiekoMQsuciJgIpkEQZ'
-$.get(gifURL3, response => {
-	const gifs = response.data;
-	// for (let i = 0; i < gifs.length; i++) {
-		console.log(gifs[1])
-		const end = gifs[0].images.downsized_medium.url;
-		$giphContainer3.prepend(`<img src="${end}">`)
 		// }
 	})
